210def4d1392f47fa6015d6218340676a6a24331
Home <Home@LAPTOP-2E6V6ACM>
2023-10-23 12:56:58 +0300

    Add project files.

---
+++ Properties/Cookie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class Cookie
+    {
+        private string flavor;
+        //private int _raising;
+        //auto Property - רק אם הקריאה וההשמה של ערכים ללא בדיקות/שינוי
+        public int Raising { get; set; }
+
+        //accessor - משתנה גישה
+        //Property
+        //מאחזר -= get
+        //קובע set
+        public string Flavor { get { return flavor; } set { if (value != "Chocolate") flavor = "Vanilla"; else flavor = value; }
+
+            //  public string GetFlavor() { return flavor; }
+            // public void SetFlavor(string flavor)
+            // { this.flavor = flavor; }
+
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cookie c = new Cookie();
+           // c.SetFlavor("Vanilla");
+           
+           
+            c.Flavor = "Straberry";
+            Console.WriteLine(c.Flavor); 
+
+        }
+    }
+}
